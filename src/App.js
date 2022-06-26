2d2c6144f9071f8fde12fd36c15b3262594e8200
Margarita Korotkova <beizmargo@gmail.com>
2022-06-26 18:01:28 +0300

    fix: add files, formatting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import {FinalForm} from './components/FinalForm/index'
 
-
-import './app.scss'
+import "./app.scss";
 
 export function App() {
   return (
     <div>
-      <header className='app__header'>
+      <header className="app__header">
 
       </header>
     </div>
   );
 }
-
